b6e0f19beef708c345a47901839bc58e8b9b5adf
“BishrJones” <bishr6jones@gmail.com>
2022-02-24 12:43:05 -0700

    Make the reset responsive

--- js/app.js
+++ js/app.js
@@ -32,28 +32,32 @@ const getRandomNum2 = (min, max) => {
 //     
 // }
 
+// make a roll cheeck function that takes two numbers
+// const rollCheck = (num1,num2) => {...}
+
 rollButton.addEventListener('click', function() {
     randomNum1 = getRandomNum1(1, 6)
     console.log('this is the random number:', randomNum1)
     randomNum2 = getRandomNum2(1, 6)
-    displayRandomNum1.innerHTML = randomNum1
-    displayRandomNum2.innerHTML = randomNum2
+    displayRandomNum1.innerText = randomNum1
+    displayRandomNum2.innerText = randomNum2
         // console.log('clicked')
 
     // create if else statement to detereming wining conditions
     if (randomNum1 + randomNum2 === 7 || randomNum1 + randomNum2 === 11) {
-        message.innerHTML = 'Nice! Keep on rolling.'
+        message.innerText = 'Nice! Keep on rolling.'
     } else if (randomNum1 + randomNum2 === 2 || randomNum1 + randomNum2 === 3 || randomNum1 + randomNum2 === 12) {
-        message.innerHTML = 'Yikes! Pass the Dice.'
+        message.innerText = 'Yikes! Pass the Dice.'
             // make conditional that all
     } else if (randomNum1 + randomNum2 === 4 || randomNum1 + randomNum2 === 5 || randomNum1 + randomNum2 === 6 || randomNum1 + randomNum2 === 8 || randomNum1 + randomNum2 === 9 || randomNum1 + randomNum2 === 10) {
-        message.innerHTML = 'roll your point again to win!'
+        message.innerText = 'roll your point again to win!'
     }
 
 })
 
 resetButton.addEventListener('click', function() {
-    displayRandomNum1.innerHTML.remove()
+    displayRandomNum1.innerText = ''
+    displayRandomNum2.innerText = ''
 })
 
 
